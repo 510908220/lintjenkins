3fbb4ca64a6d60304041d6200c9103d53c1d5f86
root <root@ashe>
2017-06-11 18:25:00 +0000

    add code

--- travis_pypi_setup.py
+++ travis_pypi_setup.py
@@ -12,7 +12,8 @@ import yaml
 from cryptography.hazmat.primitives.serialization import load_pem_public_key
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric.padding import PKCS1v15
-
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 
 try:
     from urllib import urlopen
