819b0dc6f1aa145c9fa5a7206503a327e9e2d17e
root <root@ashe>
2017-06-14 23:32:38 +0000

    add author info

--- lintjenkins/__init__.py
+++ lintjenkins/__init__.py
@@ -1,4 +1,9 @@
 # -*- encoding: utf-8 -*-
+"""Top-level package for lintjenkins."""
+
+__author__ = """westdoorblowcola"""
+__email__ = '510908220@qq.com'
+__version__ = '0.1.0'
 
 
 import pendulum
